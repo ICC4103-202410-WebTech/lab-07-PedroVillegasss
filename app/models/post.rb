ab938905803d215b55c44addbcb22278ca856a69
PedroVillegasss <ppvillegas@miuandes.cl>
2024-04-22 18:14:46 -0400

    validations completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,17 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :tags, :through => :post_tags
+
+  validates :title, presence :true, message: "The post should have a title"
+  validates :content, presence :true, message: "The post should have a content"
+  validates :user_id, presence :true, message: "The post should belong to a user"
+  validates :answers_count, numericality : { greater_than_or_equal_to: 0, message: "The post should have equal or greater than 0 answers_count" }
+  validates :likes_count, numericality : { greater_than_or_equal_to: 0, message: "The post should have equal or greater than 0 likes_count" }
+
+  before_validation :set_published_at_to_default
+
+  private
+  def set_published_at_to_default
+    self.published_at = Time.current if published_at.nil?
+  end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,5 @@
 class Tag < ApplicationRecord
   has_many :posts, :through => :post_tags
+
+  validates :name, presence: true, uniqueness: true, message: "The tag should have a name and must be unique"
 end
